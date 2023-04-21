7b76e041c3ce81bff3a790467eaf90209aa941d9
Alexandre Lecoeur <alexandre.lecoeur.dev@gmail.com>
2023-04-21 11:49:20 +0200

    delete place done

--- src/main/java/org/example/Ihm.java
+++ src/main/java/org/example/Ihm.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class Ihm {
     static Scanner sc = new Scanner(System.in);
-    static ArrayList allPlaces = new ArrayList<>();
+    static List<Place> allPlaces = new ArrayList<>();
     public static void menu() {
 
         //Affichage du menu
-        String choises[] = {"1- Ajouter, modifier ou supprimer un lieu", "2- Ajouter, modifier ou supprimer un événement", "3- Ajouter, modifier ou supprimer un client", "4- Acheter un billet", "5- Annuler un achat de billet", "6- Afficher la liste des événements dispônibles ", "6- Afficher la liste des billets achetés par un client ", "0- Quitter"};
+        String choises[] = {"1- Ajouter, modifier ou supprimer un lieu", "2- Ajouter, modifier ou supprimer un événement", "3- Ajouter, modifier ou supprimer un client", "4- Acheter un billet", "5- Annuler un achat de billet", "6- Afficher la liste des événements disponibles ", "6- Afficher la liste des billets achetés par un client ", "0- Quitter"};
         System.out.println("Bienvenue sur le service de réservations");
         for (String c : choises) {
             System.out.println(c);
@@ -29,14 +29,28 @@ public class Ihm {
                     case 1:
                         System.out.println("Veuillez saisir le nom du lieu");
                         String placeName = sc.next();
-                        System.out.println("Veuillez saisir l'adresse'");
+                        System.out.println("Veuillez saisir l'adresse");
                         String placeAddress = sc.next();
                         System.out.println("Veuillez saisir la capacité");
                         int placeCapacity = sc.nextInt();
                         allPlaces.add(new Place(placeName, placeAddress, placeCapacity));
-                        if (!allPlaces.isEmpty()) {
-                            System.out.println("Ajout effectué");
+                        System.out.println(allPlaces.toString());
+                        menu();
+                    case 2:
+
+                    case 3:
+                        System.out.println("Veuillez saisir le nom du lieu");
+                        String placeNameForDelete = sc.next();
+                        System.out.println("Veuillez saisir le nom du lieu");
+                        String placeAddressForDelete = sc.next();
+                        for (int i = 0; i < allPlaces.size(); i++) {
+                            if(allPlaces.get(i).getName().equals(placeNameForDelete) & allPlaces.get(i).getAddress().equals(placeAddressForDelete)) {
+                                allPlaces.remove(allPlaces.get(i));
+                                System.out.println(placeNameForDelete +" de "+ placeAddressForDelete + " a été supprimé");
+                            }
                         }
+                        System.out.println(allPlaces.toString());
+                        menu();
 
                 }
         }
